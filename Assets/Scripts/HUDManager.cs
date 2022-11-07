fb398d0e310b72da142e0c6fc31fc640574db3b2
Eric Mofield <ericmofield@gmail.com>
2022-11-07 00:00:01 -0500

    Update progress UI

--- Assets/Scripts/HUDManager.cs
+++ Assets/Scripts/HUDManager.cs
@@ -1,26 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class HUDManager : MonoBehaviour
 {
-    [Header("Mass")]
-    [SerializeField] private TMP_Text _massUIText;
+    [Header("Progress")]
+    [SerializeField] private TMP_Text _massTextBehind;
+    [SerializeField] private TMP_Text _massTextFront;
+    [SerializeField] private TMP_Text _winAmountBehind;
+    [SerializeField] private TMP_Text _winAmountFront;
+    [SerializeField] private Slider _progressSlider;
+
+    [Space]
+    public float TempWinValue;
+
+    [Header("Listening to...")]
     [SerializeField] private FloatEventSO _massChangeEvent;
+
+    private void Start()
+    {
+        _winAmountBehind.text = $"/ {TempWinValue}";
+        _winAmountFront.text = $"/ {TempWinValue}";
+    }
 
     private void OnEnable()
     {
-        _massChangeEvent.OnEventRaised += UpdateText;
+        _massChangeEvent.OnEventRaised += ProcessMassChange;
     }
 
     private void OnDisable()
     {
-        _massChangeEvent.OnEventRaised -= UpdateText;
+        _massChangeEvent.OnEventRaised += ProcessMassChange;
     }
 
-    private void UpdateText(float value)
+    private void ProcessMassChange(float value)
     {
-        _massUIText.text = $"Mass: {value}";
+        float rounded = Mathf.Round(value * 100f) / 100f;
+        _massTextBehind.text = $"Size: {rounded}";
+        _massTextFront.text = $"Size: {rounded}";
+        _progressSlider.value = 1 / (TempWinValue / value);
     }
 }
