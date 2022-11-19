1496a0a01c47befd05017cb190f257f12d4a4126
Eric Mofield <ericmofield@gmail.com>
2022-11-18 20:55:14 -0500

    Add pause gameplay SO

---
+++ Assets/Scripts/ScriptableObjects/PauseGameplayEventSO.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+
+[CreateAssetMenu(fileName ="PauseEvent", menuName = "Game Off/Pause event")]
+public class PauseGameplayEventSO : ScriptableObject
+{
+    public UnityAction<bool, bool> OnEventRaised;
+
+    public void Raise(bool pause, bool shouldStopMovement = false, string elevator = "(Unknown)")
+    {
+        if (OnEventRaised != null)
+        {
+            OnEventRaised.Invoke(pause, shouldStopMovement);
+        }
+        else
+        {
+            Debug.LogWarning($"{elevator} raised {name} (pause: {pause}," +
+                $" shouldStopMovement: {shouldStopMovement}) " +
+                $"but no one listens.");
+        }
+    }
+
+    public void Raise(bool pause, string elevator = "(Unknown)")
+    {
+        Raise(pause, false, elevator);
+    }
+}
