828995dbaa8e9574d7f643e621d95d0da9fc3ca2
Eric Mofield <ericmofield@gmail.com>
2022-11-21 23:42:51 -0500

    Add new event SO types

---
+++ Assets/Scripts/ScriptableObjects/BoolEventSO.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu(fileName = "Bool_NAME" , menuName = "Game Off/Bool Event")]
+public class BoolEventSO : ScriptableObject
+{
+    public UnityAction<float> OnEventRaised;
+
+    public void Raise(float value, string elevator = "(Unknown)")
+    {
+        Debug.Log($"{elevator} raised {name}");
+        if (OnEventRaised != null) OnEventRaised.Invoke(value);
+        else Debug.LogWarning($"{elevator} raised {name} but no one listens.");
+    }
+}
---
+++ Assets/Scripts/ScriptableObjects/IntEventSO.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu(fileName = "Int_NAME"
+    , menuName = "Game Off/Int Event")]
+public class IntEventSO : ScriptableObject
+{
+    public UnityAction<int> OnEventRaised;
+
+    public void Raise(int value, string elevator = "(Unknown)")
+    {
+        Debug.Log($"{elevator} raised {name}");
+        if (OnEventRaised != null) OnEventRaised.Invoke(value);
+        else Debug.LogWarning($"{elevator} raised {name} but no one listens.");
+    }
+}
