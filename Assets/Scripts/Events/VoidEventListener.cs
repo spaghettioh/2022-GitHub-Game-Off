f81720b71b5d56a24d7c58c9721eda8a6954460a
Eric Mofield <ericmofield@gmail.com>
2022-11-21 23:42:00 -0500

    Update VoidEventListener.cs

--- Assets/Scripts/Events/VoidEventListener.cs
+++ Assets/Scripts/Events/VoidEventListener.cs
@@ -3,10 +3,12 @@ using UnityEngine.Events;
 
 public class VoidEventListener : MonoBehaviour
 {
+	[SerializeField] private bool _testNow;
+
 	[Header("Listening to...")]
 	[SerializeField] private VoidEventSO _voidEventChannel;
 
-	public UnityEvent OnEventRaised;
+	[SerializeField] private UnityEvent OnEventRaised;
 
 	private void OnEnable() => _voidEventChannel.OnEventRaised += Respond;
 
