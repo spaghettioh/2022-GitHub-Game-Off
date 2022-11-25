4c4e00d34d52d6520e859e63f2cd6ee7181096c8
Eric Mofield <ericmofield@gmail.com>
2022-11-25 15:23:07 -0500

    Update level 1 cutscene

--- Assets/Scripts/Cutscenes/CutsceneScreenSO.cs
+++ Assets/Scripts/Cutscenes/CutsceneScreenSO.cs
@@ -1,36 +1,45 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-[CreateAssetMenu(
-    fileName = "Cutscene_NAME_Screen_00", menuName = "Game Off/Cutscene Screen")]
+[CreateAssetMenu(fileName = "Cutscene_NAME_Screen_00",
+    menuName = "Game Off/Cutscene Screen")]
 public class CutsceneScreenSO : ScriptableObject
 {
     [System.Serializable]
     public struct TextAndSound
     {
-        public TextAndSound(string text)
-        {
-            Text = text;
-            CharacterSound = default;
-            BlockSound = default;
-            WaitTime = 0f;
-        }
+        [Header("Required")]
+        [Tooltip(
+            "The text to show.")]
+        [TextArea(5, 5)]
+        public string Text;
 
-        [TextArea(5, 5)] public string Text;
-        public AudioCueSO CharacterSound;
-        [Space]
+        [Header("Optional")]
+        [Tooltip("Plays when block is shown")]
         public AudioCueSO BlockSound;
+        [Tooltip("Custom text character sound")]
+        public AudioCueSO CharacterSound;
+        [Tooltip("Custom wait time when text is finished")]
         public float WaitTime;
+
     }
-
-    [SerializeField] private CutsceneTestEventSO _testEvent;
-    [field: SerializeField] public Sprite Graphic { get; private set; }
-
+    [field: SerializeField]
+    public Sprite Graphic { get; private set; }
     [field: SerializeField]
     public List<TextAndSound> TextBlocks { get; private set; }
 
+    [Tooltip("For use with Cutscene_Tester.scene")]
+    [SerializeField]
+    private CutsceneTestEventSO _testEvent;
+    [Tooltip("Click to fire the tester event")]
+    [SerializeField]
+    private bool _validateNow;
+
     private void OnValidate()
     {
+        _validateNow = false;
+
         if (TextBlocks.Count == 0)
         {
             throw new System.Exception($"{name} TextAndSounds can't be empty!");
